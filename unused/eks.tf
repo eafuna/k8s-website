bb6aac729c798f705e8f8c62002fd4365bc765fa
natut <infinity.enoch@gmail.com>
2023-10-12 12:25:02 +0800

    moving unused tfs to skip during plan

---
+++ unused/eks.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "k8s_website" {
+  name = "${var.cluster_name}-iam-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "k8s_website-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.k8s_website.name
+}
+
+resource "aws_eks_cluster" "k8s_website" {
+  name = var.cluster_name
+  role_arn = aws_iam_role.k8s_website.arn
+
+  # using AWS default k8 version
+  # version = XXX
+
+  vpc_config {
+    endpoint_private_access = true
+    endpoint_public_access  = false
+    subnet_ids = concat(
+      values(aws_subnet.k8s_public_subnet).*.id,
+      values(aws_subnet.k8s_private_subnet).*.id
+    )
+
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.k8s_website-AmazonEKSClusterPolicy]
+}
