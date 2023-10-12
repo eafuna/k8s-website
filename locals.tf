4a712c4677ac39dceb08c063a366399c080ba886
natut <infinity.enoch@gmail.com>
2023-10-12 08:34:51 +0800

    Clean up subnets and added required subnet tags to locals

--- locals.tf
+++ locals.tf
@@ -1,3 +1,9 @@
 locals {
   k8s_vpc_id = var.k8s_use_vpc_id
+
+  k8s_required_subnet_tags = {
+  #   "Name" = "Something" Display in aws console
+    "kubernetes.io/role/internal-elb"             = "1" # - required for kubernetes to discover subnets where private load balancers will be created 
+    "kubernetes.io/cluster/{$var.cluster_name}"   = "owned" # subnet owned exclussively by cluster
+  }
 }
--- subnets.tf
+++ subnets.tf
@@ -1,27 +1,5 @@
-locals {
-  # tags = {
-  #   "kubernetes.io/role/internal-elb" = "1"
-  #   "kubernetes.io/cluster/demo"      = "owned"
-  # }  
-  tags = {}
-}
 
-# resource "aws_subnet" "k8s-private-subnet" {
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = "10.0.0.0/19"
-#   availability_zone = "us-east-1a"
 
-#   tags = merge(local.tags, { Name = "private-us-east-1a" })
-  
-# }
-
-# resource "aws_subnet" "private-us-east-1b" {
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = "10.0.32.0/19"
-#   availability_zone = "us-east-1b"
-
-#   tags = merge(local.tags, { Name = "private-us-east-1b" })
-# }
 
 resource "aws_subnet" "k8s_public_subnet" {
 
@@ -31,7 +9,7 @@ resource "aws_subnet" "k8s_public_subnet" {
   map_public_ip_on_launch = true
 
   availability_zone = each.key
-  tags = merge(local.tags, { Name = each.value["subnet_tag_name"] })
+  tags = merge(local.k8s_required_subnet_tags, { Name = each.value["subnet_tag_name"] })
   
 }
 
@@ -45,34 +23,7 @@ resource "aws_subnet" "k8s_private_subnet" {
   map_public_ip_on_launch = true
 
   availability_zone = each.key
-  tags = merge(local.tags, { Name = each.value["subnet_tag_name"] })
+  tags = merge(local.k8s_required_subnet_tags, { Name = each.value["subnet_tag_name"] })
   
 }
 
-
-# resource "aws_subnet" "k8s_public_subnet" {
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = "10.0.96.0/19"
-#   availability_zone       = "us-east-1b"
-#   map_public_ip_on_launch = true
-
-#   tags = {
-#     "Name"                       = "public-us-east-1b"
-#     "kubernetes.io/role/elb"     = "1"
-#     "kubernetes.io/cluster/demo" = "owned"
-#   }
-# }
-
-
-# resource "aws_subnet" "public-us-east-1b" {
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = "10.0.96.0/19"
-#   availability_zone       = "us-east-1b"
-#   map_public_ip_on_launch = true
-
-#   tags = {
-#     "Name"                       = "public-us-east-1b"
-#     "kubernetes.io/role/elb"     = "1"
-#     "kubernetes.io/cluster/demo" = "owned"
-#   }
-# }
